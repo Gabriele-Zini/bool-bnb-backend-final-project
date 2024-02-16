966cdf3a54793a365d2743cef27f9974db3e4db0
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-02-16 16:23:11 +0100

    Initial commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,51 @@
+<div class="container mt-5 d-flex justify-content-center">
+    @vite(['resources/js/app.js'])
+    <div class="row justify-content-center w-50">
+        <div class="col-md-8">
+            <!-- Session Status -->
+            <x-auth-session-status class="mb-4" :status="session('status')" />
+
+            <form method="POST" action="{{ route('login') }}" class="mb-4">
+                @csrf
+
+                <!-- Email Address -->
+                <div class="mb-3">
+                    <x-input-label for="email" :value="__('Email')" />
+                    <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')"
+                        required autofocus autocomplete="username" />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
+
+                <!-- Password -->
+                <div class="mb-3">
+                    <x-input-label for="password" :value="__('Password')" />
+                    <x-text-input id="password" class="form-control" type="password" name="password" required
+                        autocomplete="current-password" />
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </div>
+
+                <!-- Remember Me -->
+                <div class="form-check mb-3">
+                    <input class="form-check-input" type="checkbox" id="remember_me" name="remember">
+                    <label class="form-check-label" for="remember_me">
+                        {{ __('Remember me') }}
+                    </label>
+                </div>
+
+                <div class="d-grid gap-2">
+                    <button type="submit" class="btn btn-primary">{{ __('Log in') }}</button>
+                </div>
+            </form>
+
+            @if (Route::has('password.request'))
+                <a class="btn btn-link" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}"
+                    class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+            @endif
+        </div>
+    </div>
+</div>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,48 @@
+<x-guest-layout>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+
+                    <!-- Name -->
+                    <div class="mb-3">
+                        <label for="name" class="form-label">{{ __('Name') }}</label>
+                        <input id="name" class="form-control" type="text" name="name"
+                            value="{{ old('name') }}" required autofocus autocomplete="name">
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    </div>
+
+                    <!-- Email Address -->
+                    <div class="mb-3">
+                        <label for="email" class="form-label">{{ __('Email') }}</label>
+                        <input id="email" class="form-control" type="email" name="email"
+                            value="{{ old('email') }}" required autocomplete="username">
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+
+                    <!-- Password -->
+                    <div class="mb-3">
+                        <label for="password" class="form-label">{{ __('Password') }}</label>
+                        <input id="password" class="form-control" type="password" name="password" required
+                            autocomplete="new-password">
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+
+                    <!-- Confirm Password -->
+                    <div class="mb-3">
+                        <label for="password_confirmation" class="form-label">{{ __('Confirm Password') }}</label>
+                        <input id="password_confirmation" class="form-control" type="password"
+                            name="password_confirmation" required autocomplete="new-password">
+                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                    </div>
+
+                    <div class="d-flex justify-content-between align-items-center mt-4">
+                        <a href="{{ route('login') }}" class="text-decoration-none">{{ __('Already registered?') }}</a>
+                        <button type="submit" class="btn btn-primary">{{ __('Register') }}</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</x-guest-layout>
