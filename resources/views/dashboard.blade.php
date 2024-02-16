ee71e75c43485ed89b5fdd9a2b0f274bd394f791
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-02-16 20:34:51 +0100

    restore dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,20 +1,17 @@
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header bg-white">
-                    <h2 class="font-weight-bold text-xl text-gray-800">{{ __('Dashboard') }}</h2>
-                </div>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
 
-                <div class="card-body py-12">
-                    <div class="bg-white shadow-sm rounded-lg">
-                        <div class="p-6 text-gray-900">
-                            {{ __("You're logged in!") }}
-                        </div>
-                    </div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
                 </div>
             </div>
         </div>
     </div>
-</div>
-
+</x-app-layout>
