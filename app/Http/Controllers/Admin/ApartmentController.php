5b4b40838bf32e45e5e60d2307a98a88e249f005
simonemassenti <s.massenti96@gmail.com>
2024-02-17 16:52:20 +0100

    add: index in controller

--- app/Http/Controllers/Admin/ApartmentController.php
+++ app/Http/Controllers/Admin/ApartmentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Apartment;
 use Illuminate\Http\Request;
 
 class ApartmentController extends Controller
@@ -12,7 +13,10 @@ class ApartmentController extends Controller
      */
     public function index()
     {
-        //
+        // $perPage = 10;
+        $apartments = Apartment::all();
+
+        return view('admin.apartments.index', compact('apartments'));
     }
 
     /**
