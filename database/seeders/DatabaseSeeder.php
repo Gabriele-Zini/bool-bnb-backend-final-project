bdcc1cbccbaac49ed4f90b989783ed736aa46d4c
andrearicciotti <ricciotti.andrea1993@gmail.com>
2024-02-19 10:44:25 +0100

    add: dbSeed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,13 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Apartment_info;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    use WithoutModelEvents;
     /**
      * Seed the application's database.
      */
@@ -18,5 +20,14 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([
+            ApartmentsTableSeeder::class,
+            MessagesTableSeeder::class,
+            ServicesTableSeeder::class,
+            SponsorshipsTableSeeder::class,
+            ViewsTableSeeder::class,
+            Apartment_infoTableSeeder::class,
+            ApartmentSponsorshipSeeder::class
+        ]);
     }
 }
