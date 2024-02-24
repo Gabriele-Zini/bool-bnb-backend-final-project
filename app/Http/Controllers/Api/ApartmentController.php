42078af042239f3c81aa33c81d627ee3d45b0ad9
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-02-24 09:52:52 +0100

    add: first filters

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -15,18 +15,19 @@ class ApartmentController extends Controller
     {
         $apartmentsQuery = Apartment::with(['services', 'apartment_info', 'user', 'images'])->where('visibility', "=", 1);
         $services=Service::all();
-        
+
         if($request->has('services')){
-            // $servicesSelected = $request->get('services');
-            //    $apartmentsQuery = $apartmentsQuery->join('apartment_service', 'services.id', '=','apartment_service.apartment_id')->join('apartments','apartment_service.id', '=','apartments.id')->select('apartmens.*');
-            //    foreach($servicesSelected as $service){
-            //       $apartmentsQuery = $apartmentsQuery->where($service, '=', 'service.id');
-            //    }
+           /*  $servicesSelected = $request->get('services');
+               $apartmentsQuery = $apartmentsQuery->join('apartment_service', 'services.id', '=','apartment_service.apartment_id')->join('apartments','apartment_service.id', '=','apartments.id')->select('apartmens.*');
+               foreach($servicesSelected as $service){
+                  $apartmentsQuery = $apartmentsQuery->where($service, '=', 'service.id');
+               } */
+
         }
             $apartments = $apartmentsQuery->paginate(20);
-        
-        
-        
+
+
+
 
         if ($apartments) {
             return response()->json(
@@ -49,7 +50,7 @@ class ApartmentController extends Controller
     public function show($slug)
     {
         $apartment = Apartment::with(['services', 'apartment_info', 'images'])->where('slug', $slug)->first();
-        
+
         if ($apartment) {
             return response()->json([
                     'result' => $apartment,
@@ -62,4 +63,42 @@ class ApartmentController extends Controller
                 ]);
         }
     }
+
+    public function getFilteredApartments(Request $request)
+    {
+
+        $query = Apartment::where('visibility', 1);
+
+        if ($request->has('num_beds')) {
+            $query->whereHas('apartment_info', function ($q) use ($request) {
+                $q->where('num_beds', '>=', $request->num_beds);
+            });
+        }
+
+        if ($request->has('num_rooms')) {
+            $query->whereHas('apartment_info', function ($q) use ($request) {
+                $q->where('num_rooms', '>=', $request->num_rooms);
+            });
+        }
+
+        if ($request->has('num_bathrooms')) {
+            $query->whereHas('apartment_info', function ($q) use ($request) {
+                $q->where('num_bathrooms', '>=', $request->num_bathrooms);
+            });
+        }
+
+        if ($request->has('mt_square')) {
+            $query->whereHas('apartment_info', function ($q) use ($request) {
+                $q->where('mt_square', '>=', $request->mt_square);
+            });
+        }
+
+        $query->with('apartment_info', 'services');
+
+        $apartments = $query->get();
+
+        return response()->json(['apartments' => $apartments]);
+    }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/apartments', [ApartmentController::class, 'index']);
 Route::get('/apartments/{slug}', [ApartmentController::class, 'show']);
+Route::get('/get-apartments', [ApartmentController::class, 'getFilteredApartments']);
