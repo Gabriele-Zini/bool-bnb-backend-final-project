ce2049a6132e72becea4c1b06553131897890788
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-02-27 20:09:05 +0100

    add: some fk results

--- app/Http/Controllers/BraintreeController.php
+++ app/Http/Controllers/BraintreeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Braintree\Gateway;
 
 use Illuminate\Http\Request;
@@ -17,21 +18,36 @@ class BraintreeController extends Controller
             'privateKey' => 'eeaae14db9451cbf55bec4b30dd00b23'
         ]);
 
-        if($request->input('nonce') != null){
-            var_dump($request->input('nonce'));
-            $nonceFromTheClient = $request->input('nonce');
-
-            $gateway->transaction()->sale([
-                'amount' => '10.00',
-                'paymentMethodNonce' => $nonceFromTheClient,
-                'options' => [
-                    'submitForSettlement' => True
-                ]
-            ]);
-            return view ('dashboard');
-        }
         $clientToken = $gateway->clientToken()->generate();
         return view('admin.braintree', ['token' => $clientToken]);
     }
 
+    public function processTransaction(Request $request)
+    {
+        $gateway = new \Braintree\Gateway([
+            'environment' => 'sandbox',
+            'merchantId' => 'ympkctn64j5ws654',
+            'publicKey' => '4g4bdtcpcxxtz3xj',
+            'privateKey' => 'eeaae14db9451cbf55bec4b30dd00b23'
+        ]);
+
+        $nonceFromTheClient = $request->input('nonce');
+        $nonceFromTheClient = $request->input('nonce');
+
+        $result = $gateway->transaction()->sale([
+            'amount' => '10.00',
+            'paymentMethodNonce' => $nonceFromTheClient,
+            'options' => [
+                'submitForSettlement' => True
+            ]
+        ]);
+
+
+            $data = [
+                'success' => false,
+                'message' => "Transazione Fallita!!",
+                'result'=> $result,
+            ];
+            return response()->json($data, 200);
+    }
 }
