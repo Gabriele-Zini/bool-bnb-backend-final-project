4577e34ac817956502774f2c652d31f22bec9fe6
simonemassenti <s.massenti@gmail.com>
2024-02-28 12:28:46 +0100

    add: success in all payments!!!

--- app/Http/Controllers/BraintreeController.php
+++ app/Http/Controllers/BraintreeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ApartmentSponsorship;
 use Braintree\Gateway;
 
 use Illuminate\Http\Request;
@@ -14,10 +15,10 @@ class BraintreeController extends Controller
         $apartmentSlug = $request->apartment;
         $sponsorship = $request->sponsorship_data;
         $gateway = new Gateway([
-            'environment' => 'sandbox',
-            'merchantId' => '7snbfz9sqqk62yfq',
-            'publicKey' => 'v467n9qddqbrbq2b',
-            'privateKey' => 'b9430c58b1145441329fcf838562b6ef'
+            'environment' => env('BRAINTREE_ENVIRONMENT'),
+            'merchantId' => env('BRAINTREE_MERCHANT_ID'),
+            'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
+            'privateKey' => env('BRAINTREE_PRIVATE_KEY'),
         ]);
 
         $clientToken = $gateway->clientToken()->generate();
@@ -29,15 +30,17 @@ class BraintreeController extends Controller
     {
        
         $gateway = new Gateway([
-            'environment' => 'sandbox',
-            'merchantId' => '7snbfz9sqqk62yfq',
-            'publicKey' => 'v467n9qddqbrbq2b',
-            'privateKey' => 'b9430c58b1145441329fcf838562b6ef'
+            'environment' => env('BRAINTREE_ENVIRONMENT'),
+            'merchantId' => env('BRAINTREE_MERCHANT_ID'),
+            'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
+            'privateKey' => env('BRAINTREE_PRIVATE_KEY'),
         ]);
         $nonceFromTheClient = $request->input('nonce');
 
+        $data = $request->all();
+        $info = $data['info'];
         $result = $gateway->transaction()->sale([
-            'amount' => '11.00',
+            'amount' => $info['price'],
             'paymentMethodNonce' => $nonceFromTheClient,
             'options' => [
                 'submitForSettlement' => true
@@ -45,7 +48,15 @@ class BraintreeController extends Controller
         ]);
 
         if ($result->success) {
+            $apartment_sponsorship = new ApartmentSponsorship;
+            $apartment_sponsorship->start_date = $info['start_date'];
+            $apartment_sponsorship->expiration_date = $info['expiration_date'];
+            $apartment_sponsorship->sponsorship_id =  $info['sponsorship_id'];
+            $apartment_sponsorship->apartment_id = $info['apartment_id'];
+            $apartment_sponsorship->save();
+
             $data = [
+                'request' => $info['price'],
                 'success' => true,
                 'message' => "Transazione eseguita con Successo!"
             ];
--- resources/views/admin/braintree.blade.php
+++ resources/views/admin/braintree.blade.php
@@ -7,14 +7,15 @@
     <div class="py-12">
         @csrf
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <input type="hidden" name="apartment" value="{{ $apartment }}">
-        <input type="hidden" name="price" value="{{ $sponsorship['price'] }}">
-        <input type="hidden" name="start_date" value="{{ $sponsorship['start_date'] }}">
-        <input type="hidden" name="expiration_date" value="{{ $sponsorship['expiration_date'] }}">
-        <input type="hidden" name="sponsorship_id" value="{{ $sponsorship['sponsorship_id'] }}">
-        <input type="hidden" name="apartment_id" value="{{ $sponsorship['apartment_id'] }}">
+        <input type="hidden" id="apartment" name="apartment" value="{{ $apartment }}">
+        <input type="hidden" id="price" name="price" value="{{ $sponsorship['price'] }}">
+        <input type="hidden" id="start_date" name="start_date" value="{{ $sponsorship['start_date'] }}">
+        <input type="hidden" id="expiration_date" name="expiration_date" value="{{ $sponsorship['expiration_date'] }}">
+        <input type="hidden" id="sponsorship_id" name="sponsorship_id" value="{{ $sponsorship['sponsorship_id'] }}">
+        <input type="hidden" id="apartment_id" name="apartment_id" value="{{ $sponsorship['apartment_id'] }}">
         <div id="dropin-container"></div>
         <div>
+            
             <a id="submit-button" class="btn btn-sm btn-success">Submit payment</a>
         </div>
 
@@ -22,16 +23,25 @@
     
 
     <script>
-        let route = 'http://localhost:8000/transaction'
+        let info = {
+            price: document.querySelector('#price').value,
+            slug: document.querySelector('#apartment').value,
+            start_date: document.querySelector('#start_date').value,
+            expiration_date: document.querySelector('#expiration_date').value,
+            sponsorship_id: document.querySelector('#sponsorship_id').value,
+            apartment_id: document.querySelector('#apartment_id').value
+        };
         let button = document.querySelector('#submit-button');
             braintree.dropin.create({
                 authorization: '{{ $token }}',
-                container: '#dropin-container'
+                container: '#dropin-container',
+                
             }, function(createErr, instance) {
                 button.addEventListener('click', function() {
                     instance.requestPaymentMethod(function(err, payload) {
                         axios.post('{{ route('transaction') }}', {
-                                nonce: payload.nonce
+                                nonce: payload.nonce,
+                                info: info
                             }, {
                                 headers: {
                                     'X-CSRF-TOKEN': document.querySelector(
@@ -40,6 +50,7 @@
                             })
                             .then(function(response) {
                                 console.log('success', payload.nonce);
+                                console.log(response);
                             })
                             .catch(function(error) {
                                 console.log('error', payload.nonce);
