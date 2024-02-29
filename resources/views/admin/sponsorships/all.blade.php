84c7ea25ba6f13de401db520c01a45f3b7ab5427
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-02-29 22:27:57 +0100

    add: status sponsorships

--- resources/views/admin/sponsorships/all.blade.php
+++ resources/views/admin/sponsorships/all.blade.php
@@ -15,6 +15,7 @@
                         <tr>
                             <th scope="col">#</th>
                             <th scope="col">Sponsorship</th>
+                            <th scope="col">Status</th>
                             <th scope="col">Start Date</th>
                             <th scope="col">Expiration Date</th>
                             <th scope="col">Purchase</th>
@@ -26,6 +27,18 @@
                             <tr>
                                 <th scope="row">{{ $count }}</th>
                                 <td>{{ ucfirst($item->name) }}</td>
+                                <td>
+
+                                    @if (round((strtotime($item->expiration_date) - time()) / 3600) < 0)
+                                        <span class="badge bg-danger">Expired</span>
+                                    @elseif (round((strtotime($item->expiration_date) - time()) / 3600) > 0)
+                                        @if (round((strtotime($item->start_date) - time()) / 3600) > 0)
+                                            <span class="badge bg-success">Not yet started </span>
+                                        @else
+                                            <span class="badge bg-warning">Started</span>
+                                        @endif
+                                    @endif
+                                </td>
                                 <td>{{ date('d/m/Y', strtotime($item->start_date)) }}</td>
                                 <td>{{ date('d/m/Y', strtotime($item->expiration_date)) }}</td>
                                 <td><a class="btn btn-warning" href="{{ route('sponsorships.index', ['apartment'=>$item->slug])}}">purchase</a></td>
