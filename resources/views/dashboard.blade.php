452e3ba70434928fe4aa8e34a8f1ea77f918755e
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-03-02 03:38:20 +0100

    style to dashboard message

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,10 +2,8 @@
     <div class="py-12 d-flex flex-column align-items-center">
 
 
-        <div>
-            <div class="fw-bold my-5">
+            <div class="card ms_bg-card my-5 d-flex justify-content-center fw-bold ms_font-color p-4" style="height: 100px">
                 {{ Auth::user()->name }} {{ Auth::user()->lastname }} {{ __("you're logged in!") }}
             </div>
-        </div>
     </div>
 </x-app-layout>
