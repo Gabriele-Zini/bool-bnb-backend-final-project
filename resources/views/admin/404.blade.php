807560549d45cc18d88946c4bb692b0cc672fa27
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-03-03 19:28:15 +0100

    add: custom 404 page

---
+++ resources/views/admin/404.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container" >
+        <div class="d-flex justify-content-center align-items-center" style="min-height: 500px">
+            <div class="card col-12 col-md-8 col-lg6 ms_bg-card d-flex align-items-center justify-content-center shadow" style=" min-height:320px">
+                <div class="card ms_bg-small-card w-75v d-flex justify-content-center align-items-center p-5 shadow"  style="height:100px">
+                    <h4>
+                        Error 404: page not found
+                     </h4>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,10 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+Route::fallback(function () {
+    return view('admin.404');
+});
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
